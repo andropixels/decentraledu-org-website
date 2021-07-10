5c47f48c2e6543991fa38491197a334930ec533e
Kresna Sucandra <kresnasucandra@gmail.com>
2021-07-10 12:55:39 +0800

    Adding Footer section

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Contribute from "./components/pages/Contribute";
 import About from "./components/pages/About";
 import SignUp from "./components/pages/SignUp";
 import Whitepaper from "components/pages/Whitepaper";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/sign-up" exact component={SignUp} />
           <Route path="/whitepaper" exact component={Whitepaper} />
         </Switch>
+        <Footer />
       </Router>
     </>
   );
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,12 +6,7 @@ import "./HeroSection.css";
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video
-        src="https://github.com/Decentraledu/decentraledu-org-website/blob/959c4ec128a2f4b0de46471ac0d9fdee24037ade/src/assets/videos/video-2.mp4"
-        autoPlay
-        loop
-        muted
-      />
+      <video src="../assets/videos/video-2.mp4" autoPlay loop muted />
       <h1>Decentralized Education Metaverse</h1>
       <p>
         New form of online education in fun, transparent and decentralized way.
